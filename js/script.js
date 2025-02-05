98d9b1e2fe873377987fda5c545981fd089cbf1d
Md. Seam <seam5559@gmail.com>
2025-02-05 21:48:01 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLastCharacter() {
+  display.value = display.value.slice(0, -1);
+}
